ef65d1c0dcd7ca1651174913a9b109a9cb21993a
unknown <nishanahmed13913@gmail.com>
2026-02-21 13:07:50 +0600

    created notification file folder and created interface and mongoose schema

---
+++ src/app/modules/notification/notification.model.ts
@@ -0,0 +1,62 @@
+import { INotification, NotificationType } from "./notification.interface";
+import { model, Schema } from "mongoose";
+
+
+
+const NotificationSchema = new Schema<INotification>(
+  {
+
+    user: {
+        type: Schema.Types.ObjectId,
+        required: true
+    },
+
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    body: {
+      type: String,
+      required: true,
+    },
+
+    type: {
+      type: String,
+      enum: Object.values(NotificationType),
+      required: true,
+    },
+
+    isRead: {
+        type: Boolean,
+        default: false
+    },
+
+    entityId: {
+      type: String,
+      required: true,
+      index: true,
+    },
+
+    webUrl: {
+      type: String,
+      required: true,
+    },
+
+    deepLink: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+NotificationSchema.index({ user: 1, isRead: 1, createdAt: -1 });
+
+export const NotificationModel = model<INotification>(
+  "notification",
+  NotificationSchema
+);
