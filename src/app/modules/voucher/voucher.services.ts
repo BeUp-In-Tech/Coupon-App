8bcb48f72c6e9c3f3134bb55ce0c9bcfb0ada151
unknown <nishanahmed13913@gmail.com>
2026-02-27 19:58:17 +0600

    created voucher create, get all voucher, get single voucher, delete single voucher, update single voucher, apply voucher

--- src/app/modules/voucher/voucher.services.ts
+++ src/app/modules/voucher/voucher.services.ts
@@ -5,6 +5,8 @@ import AppError from "../../errorHelpers/AppError";
 import { StatusCodes } from "http-status-codes";
 import { Voucher } from "./voucher.model";
 
+
+// CREATE VOUCHER
 const createVoucherService = async (user: JwtPayload, payload: IVoucher) => {
     if (user.role !== Role.ADMIN) {
         throw new AppError(StatusCodes.UNAUTHORIZED, "Only admin can create voucher");
@@ -14,8 +16,127 @@ const createVoucherService = async (user: JwtPayload, payload: IVoucher) => {
     return voucher;
 }
 
+// GET ALL VOUCHERS
+const getAllVouchersService = async () => {
+  return await Voucher.find().sort({ createdAt: -1 }).lean();
+};
+
+// GET SINGLE VOUCHER
+const getSingleVoucherService = async (voucherId: string) => {
+  // FIND VOUCHER BY ID
+  const voucher = await Voucher.findById(voucherId).lean();
+
+  if (!voucher) {
+    throw new Error("VOUCHER_NOT_FOUND");
+  }
+
+  return voucher;
+};
+
+// UPDATE SINGLE VOUCHER (ADMIN ONLY)
+const updateVoucherService = async (
+  voucherId: string,
+  payload: Partial<IVoucher>,
+  user: JwtPayload
+) => {
+  // ALLOW ONLY ADMIN
+  if (user.role !==  Role.ADMIN) {
+    throw new Error("FORBIDDEN");
+  }
+
+  const updatedVoucher = await Voucher.findByIdAndUpdate(
+    voucherId,
+    payload,
+    {
+      new: true,
+      runValidators: true,
+    }
+  ).lean();
+
+  if (!updatedVoucher) {
+    throw new Error("VOUCHER_NOT_FOUND");
+  }
+
+  return updatedVoucher;
+};
+
+
+// DELETE VOUCHER
+const deleteVoucherService = async (
+  voucherId: string,
+  userRole: string
+) => {
+  // ALLOW ONLY ADMIN
+  if (userRole !== "ADMIN") {
+    throw new Error("FORBIDDEN");
+  }
+
+  // DELETE VOUCHER
+  const deletedVoucher = await Voucher.findByIdAndDelete(voucherId).lean();
+
+  if (!deletedVoucher) {
+    throw new Error("VOUCHER_NOT_FOUND");
+  }
+
+  return deletedVoucher;
+};
+
+// REDEEM VOUCHER
+const applyVoucherService = async (user: JwtPayload, voucherCode: string) => {
+
+    if (user.role !== Role.VENDOR && user.role !== Role.ADMIN) {
+        throw new AppError(StatusCodes.FORBIDDEN, "FORBIDDEN");
+    }
+
+  // FIND ACTIVE VOUCHER
+  const voucher = await Voucher.findOne({
+    voucher_code: voucherCode.toUpperCase(),
+  });
+
+  if (!voucher) {
+    throw new Error("INVALID_VOUCHER");
+  }
+
+  // CHECK EXPIRY
+  if (voucher.voucher_validity < new Date()) {
+    throw new Error("VOUCHER_EXPIRED");
+  }
+
+  // CHECK LIMIT
+  if (voucher.voucher_limit <= 0) {
+    throw new Error("VOUCHER_LIMIT_EXCEEDED");
+  }
+
+//   // ATOMIC DECREMENT TO PREVENT RACE CONDITION
+//   const redeemedVoucher = await Voucher.findOneAndUpdate(
+//     {
+//       _id: voucher._id,
+//       voucher_limit: { $gt: 0 },
+//     },
+//     {
+//       $inc: { voucher_limit: -1 },
+//     },
+//     {
+//       new: true,
+//     }
+//   ).lean();
+
+//   if (!redeemedVoucher) {
+//     throw new Error("REDEEM_FAILED");
+//   }
+
+  return  {
+    discount_parcantage: voucher.voucher_discount,
+    voucher_code: voucher.voucher_code
+  };
+};
 
 
 export const voucherServices = {
-    createVoucherService
+    createVoucherService,
+    getAllVouchersService,
+    getSingleVoucherService,
+    updateVoucherService,
+    deleteVoucherService,
+    applyVoucherService
 }
--- src/app/modules/voucher/voucher.validate.ts
+++ src/app/modules/voucher/voucher.validate.ts
@@ -23,3 +23,33 @@ export const voucherValidationSchema = z.object({
     })
     .min(0, "Voucher limit cannot be negative"),
 });
+
+
+// UPDATE VOUCHER ZOD SCHEMA
+export const voucherUpdateValidationSchema = z.object({
+  voucher_code: z
+    .string()
+    .min(1, "Voucher code is required")
+    .trim()
+    .toUpperCase()
+    .optional(),
+
+  voucher_discount: z
+    .number({
+      message: "Voucher discount must be a number",
+    })
+    .min(0, "Discount cannot be negative")
+    .optional(),
+
+  voucher_validity: z.coerce.date({
+    message: "Voucher validity date is required",
+  })
+  .optional(),
+
+  voucher_limit: z
+    .number({
+      message: "Voucher limit must be a number",
+    })
+    .min(0, "Voucher limit cannot be negative")
+    .optional(),
+});
